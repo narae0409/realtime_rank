798e53f935b2e6efafcd69d349c2a15aa762109c
narae0409 <tkdgn821@naver.com>
2021-07-29 03:15:20 +0900

    Add Thread

--- app/src/main/java/com/example/realtime_rank/MainActivity.java
+++ app/src/main/java/com/example/realtime_rank/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.realtime_rank;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.text.util.Linkify;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     String address = "https://search.naver.com/search.naver?query=";
-    TextView Rank1;
+    String url = "https://trends.google.co.kr/trends/trendingsearches/daily?geo=KR";
+    String msg;
+    final Bundle bundle = new Bundle();
+    TextView Rank1, textView;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         Rank1 = (TextView)findViewById(R.id.rank1);
+        textView = findViewById(R.id.rank2);
 
         Rank1.setText("new text");
         //setText, compile, addLinks.scheme 모두 같은 변수로 통일하여 적용
@@ -35,6 +48,33 @@ public class MainActivity extends AppCompatActivity {
         };
 
         Linkify.addLinks(Rank1, ptn, address+"test", null, transform);
+
+        Handler handler = new Handler() {
+            public void handleMessage(Message msg) {
+                Bundle bundle = msg.getData();
+                textView.setText(bundle.getString("message"));
+            }
+        };
+
+
+        new Thread(() -> {
+            Document doc = null;
+            try {
+                doc = Jsoup.connect(url).get();
+                Element elements = doc.select(".title").first();
+                msg = elements.text();
+                bundle.putString("message", msg);
+                Message msg = handler.obtainMessage();
+                msg.setData(bundle);
+                handler.sendMessage(msg);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                textView.setText("fail");
+            }
+
+
+        }).start();
 
     }
 }
